24576f5778c0260ebca36b3e9ce52d34b5d6aa63
João Fonseca <joaofvfonseca@gmail.com>
2022-04-05 00:17:45 +0100

    Remove unnecessary includes

--- Source/Telamon/Character/Player/TelamonPlayerCharacter.cpp
+++ Source/Telamon/Character/Player/TelamonPlayerCharacter.cpp
@@ -3,9 +3,7 @@
 #include "TelamonPlayerCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
-#include "Components/InputComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
-#include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
 
 ATelamonPlayerCharacter::ATelamonPlayerCharacter()
